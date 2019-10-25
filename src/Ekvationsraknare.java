d71be12980c9496f1fd5e6ed7a4773f5e5f53838
TheDeniableToast <filipnorberg02@outlook.com>
2019-10-25 10:54:14 +0200

    added files v43 fre

--- src/Ekvationsraknare.java
+++ src/Ekvationsraknare.java
@@ -4,15 +4,18 @@ public class Ekvationsraknare {
     public static void main(String[] args) {
 
         //Variables
-        String s = JOptionPane.showInputDialog(null, "Vi ska räkna ut en ekvation med formeln ax + b = 0. Välj ditt a-värde");
+        String s = JOptionPane.showInputDialog(null, "Vi ska räkna ut en ekvation med pq-formeln. Välj ditt p-värde");
         double a = Integer.parseInt(s);
 
-        String s1 = JOptionPane.showInputDialog(null, "Välj ditt b-värde");
+        String s1 = JOptionPane.showInputDialog(null, "Välj ditt q-värde");
         double b = Integer.parseInt(s1);
 
-        double c = (-b)/(a);
+        double e = Math.sqrt((a/2) * (a/2) - b);
 
-        JOptionPane.showMessageDialog(null, "Lösningen på ekvationen ax + b = 0 är" + c);
+        double c = -(a/2) + e;
+        double d = -(a/2) - e;
+
+        JOptionPane.showMessageDialog(null, "Ditt x1-värde blev" + c + "och ditt x2-värde blev" + d);
     }
 }
 
